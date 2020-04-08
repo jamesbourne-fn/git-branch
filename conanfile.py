588729dce42b91a9f9ed93dcc0896c692473e000
James Bourne <james.bourne@foundry.com>
2020-04-08 12:44:06 +0100

    added initial files
    Added intial project files

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, CMake
+
+
+class MytestlibbConan(ConanFile):
+    name = "MyTestConanPackage"
+    version = "1.2.5"
+    url = "https://blah.co.uk"
+    url_version = "1.2.5"
+    author = "james.bourne"
+    description = "Test conan package"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+    exports_sources = "src/*", "!conanfile.py"
+    
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["MyTestConanPackage"]
---
+++ test_package/example.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Hello from the test package!!" << std::endl;
+}
