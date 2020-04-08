b7738ee728663b0b48522c06ec4c1f7a8ffe0f81
James Bourne <james.bourne@foundry.com>
2020-04-08 13:14:47 +0100

    b1-b2-active: added printer utility
    Added printer utility to the b1-b2 branch

---
+++ src/Printer.cpp
@@ -0,0 +1,6 @@
+#include "Printer.h"
+
+void Printer::print(std::ostream& os, std::string msg)
+{
+    os << msg << std::endl;
+}
---
+++ src/Printer.h
@@ -0,0 +1,19 @@
+#ifndef PRINTER_H_
+#define PRINTER_H_
+#include <iostream>
+#include <string>
+
+class Printer
+{
+public:
+	/**
+	 * This is a utility method to print to output
+	 * to a stream
+	 * @param os This is the output stream
+	 * @param msg This is the message to print
+	 */
+	void print(std::ostream& os, std::string msg)
+
+};
+
+#endif
