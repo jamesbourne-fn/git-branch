9ef3d070e07403b5fc266bf4c21f10a1de968d96
James Bourne <james.bourne@foundry.com>
2020-04-08 12:48:44 +0100

    Created utiltiy print method
    Created utility print method

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 
+/**
+ * This is a utility method to print to output
+ * to a stream
+ * @param os This is the output stream
+ * @param msg This is the message to print
+ */
+void print(std::ostream& os, std::string msg)
+{
+    os << msg << std::endl;
+}
+
+/** 
+ *  This is the main method
+ */
 int main(int argc, char *argv[]) 
 {
-	std::cout << "Hello World" << std::endl;
+    print(std::cout, "Hello World!!");
     return 0;
 }
