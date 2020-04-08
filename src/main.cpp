75f6310bfdfbd8885f7398075b462916b4b769dd
James Bourne <james.bourne@foundry.com>
2020-04-08 12:59:52 +0100

    master: added add method
    added local add method to master branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+
+/**
+ * This is a utility add method
+ * @param lhs The lhs value to add
+ * @param rhs The rhs value to add
+ * @return The result of the add 
+ */
+int add(int lhs, int rhs)
+{
+    return lhs + rhs;  
+}
 
 /**
  * This is a utility method to print to output
@@ -18,5 +30,11 @@ void print(std::ostream& os, std::string msg)
 int main(int argc, char *argv[]) 
 {
     print(std::cout, "Hello World!!");
+
+    std::stringstream ss;
+
+    ss << "4+5=" << add(4, 5);
+	print(std::cout, ss.str());
+
     return 0;
 }
