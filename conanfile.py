073cc64df2929c8e70ed099bf7241f8b9c14a3cb
James Bourne <james.bourne@foundry.com>
2020-04-08 12:47:21 +0100

    updated conan recipe
    Updated conan recipe

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,11 @@ from conans import ConanFile, CMake
 
 class MytestlibbConan(ConanFile):
     name = "MyTestConanPackage"
-    version = "1.2.5"
+    version = "1.0"
     url = "https://blah.co.uk"
     url_version = "1.2.5"
     author = "james.bourne"
-    description = "Test conan package"
+    description = "Test conan package for git branch name"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
